4fce45625172058d13012dedf5b692bac7057260
sergiocardozo <scardozo.sc@gmail.com>
2023-05-04 16:01:21 -0300

    Relevamiento Visual

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'alarmaRoboPPS',
+  appId: 'com.relevamientovisualpps.app',
+  appName: 'RelevamientoVisualPPS',
   webDir: 'www',
   bundledWebRuntime: false,
   "plugins": {
